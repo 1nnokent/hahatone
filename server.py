1b3e1c82dead0fa38debd5b8544c9883e566bfcf
Camus1960 <artem.abramin@gmail.com>
2025-02-28 17:02:49 +0300

    dvdsvdsbvbdsv

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 import db_requests as dr
 import algorithms as al
 import sqlite3 as sq
+import csv
 
 app = Flask(__name__, template_folder="templates")
 app.secret_key = 'dj09(WJF*(#WsoJ#*fs'
-
 
 @app.route('/')
 def main_page():
@@ -53,6 +53,48 @@ def statistics_3_page():
         #получить данные, используя фильтры
         return render_template('statistics_3.html') #с данными
 
+#массив всех людей
+arr = []
+with open('moscow.txt', 'r', encoding='utf-8') as f:
+    count = 0
+    #print('МОСКВА')
+    for line in f:
+        line = line.split(';')
+        if count >= 1:
+            place = line[0]
+            full_name = line[1]
+            full_name = full_name.split(" ")
+            first_name = full_name[1]
+            last_name = full_name[0]
+            study_class = int(line[2])
+            school = line[3]
+            score = int(line[12])
+            role = line[13]
+            arr.append([first_name, last_name, study_class, school, score])
+            #print(place, first_name, last_name, study_class, school, score, role)
+        count += 1
+
+with open('piter.txt', 'r', encoding='utf-8') as f:
+    count = 0
+    #print('ПИТЕР')
+    for line in f:
+        line = line.split(';')
+        if count >= 1:
+            place = line[0]
+            full_name = line[1]
+            full_name = full_name.split(" ")
+            first_name = full_name[1]
+            last_name = full_name[0]
+            study_class = full_name[4]
+            s = full_name[3]
+            school = s[1:len(s) - 1]
+            score = int(line[len(line) - 1])
+            arr.append([first_name, last_name, study_class, school, score])
+            #print(place,first_name, last_name, study_class, school, score)
+        count += 1
+
+for row in arr:
+    print(row)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
