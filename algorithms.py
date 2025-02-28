341e99f149b0a0292ecf35cc71741c1a2db560d1
1nnokent <maxlynch2007@gmail.com>
2025-02-28 14:51:36 +0300

    ерерыл бд

---
+++ algorithms.py
@@ -0,0 +1,18 @@
+def get_school(person):
+
+
+def parse_region_statistics(st):
+    place_start = 1
+    parsed_st = []
+    for i in range(len(st)):
+        place_finish = place_start
+        j = i + 1
+        while st[j][1] == st[i][1]:
+            place_finish += 1
+            j += 1
+        for k in range(i, j):
+            parsed_st.append((place_start, place_finish, st[k][0], st[k][1]))
+        place_start = place_finish + 1
+    return parsed_st
+
+
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3 as sq
 
 app = Flask(__name__, template_folder="templates")
-app.secret_key = 'super_secret_key'
+app.secret_key = 'dj09(WJF*(#WsoJ#*fs'
 
 @app.route('/')
 def main_page():
@@ -10,7 +10,8 @@ def main_page():
 
 @app.route('/students/<student_id>')
 def student_page():
-    return render_template('student.html')
+    kwargs = dict()
+    return render_template('student.html', **kwargs)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
