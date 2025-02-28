6dca99bf6da94a332f4cfacddc0501e3327d9dc8
ffPerchik <zhenyapl55@gmail.com>
2025-02-28 15:41:27 +0300

    Функция

--- db_requests.py
+++ db_requests.py
@@ -5,15 +5,12 @@ from flask import Flask, render_template, request
 import sqlite3 as sq
 from datetime import datetime
 
-
 connect = sq.connect('db/base.db', check_same_thread=False)
 cursor = connect.cursor()
-
 
 def sql_execute(sql_request):
     ret = cursor.execute(sql_request)
     return ret
-
 
 def get_school(first_name, middle_name, second_name):
     sql_req = f"""
@@ -30,6 +27,23 @@ def get_school(first_name, middle_name, second_name):
             AND second_name = {second_name}  
     """
     return sql_execute(sql_req)
+
+
+def get_region_statistic(role):
+    sql_req = f"""
+        SELECT 
+            region_name, COUNT(*) 
+        FROM 
+            students INNER JOIN schools 
+            ON students.school_id = schools.school_id 
+            INNER JOIN regions 
+            ON schools.school_region_id = regions.region_id 
+        WHERE 
+            category = '{role}' 
+        GROUP BY region_name
+    """
+    result = sql_execute(sql_req)
+    return result.fetchall()
 
 
 def get_student_data(student_id):
